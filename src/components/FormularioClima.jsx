acbb2d9f1069c6b26ec1666a4bdd5f8b573216e3
Nahuel Rodriguez <nahufed@gmail.com>
2023-06-09 02:06:13 -0300

    agreada logica

--- src/components/FormularioClima.jsx
+++ src/components/FormularioClima.jsx
@@ -1,30 +1,98 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Form, InputGroup } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 const FormularioClima = () => {
-
-    const {
-        register,
-        formState:{errors},
-        handleSubmit
-    } = useForm();
+    const [localizacion,setLocalizacion]=useState([{}])
+    const [clima,setClima]=useState({})
 
 
-const onSubmit= (data) => {
-    console.log(data)    
+    let paisElegido
+    let ciudadElegida
+    let tipodeclima
+    let viento
+    let velocidad
+    let grados 
+    if( !clima || !clima.wind || !clima.wind.deg){
+        viento = ''
+        velocidad= ''
+        grados = ''
+    }else{
+        grados = clima.wind.deg
+       velocidad=  clima.wind.speed
+    }
+    if( !clima || !clima.weather){
+        tipodeclima = ''
+    }else{
+        tipodeclima = clima.weather[0].main
+    }
+    
+    const onSubmit= (data) => {
+        paisElegido=data.pais
+        ciudadElegida=data.ciudad
+        console.log(paisElegido)
+        console.log(ciudadElegida)
+        consultarApiGeolocalizacion()         
+       
 
+
+     }
+    
+    
+        const {
+            register,
+            formState:{errors},
+            handleSubmit
+        } = useForm();
+
+        useEffect(() => {
+            if (localizacion.lat && localizacion.lon) {
+              consultarApiClima();
+            }
+          }, [localizacion]);
+
+
+
+
+const consultarApiGeolocalizacion = async () => {
+    try{
+        const respuesta = await fetch(
+            `http://api.openweathermap.org/geo/1.0/direct?q=${ciudadElegida},${paisElegido}&limit=1&appid=884ba21d94e5eefa516fb60f42237962`
+        )
+        const datos = await respuesta.json();        
+        setLocalizacion(datos[0])                
+        consultarApiClima()
+    }
+    catch(err){
+        console.log(err)
+    }
+    
+}
+
+const consultarApiClima = async () => {
+    
+    try{
+        const respuesta = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${localizacion.lat}&lon=${localizacion.lon}&appid=884ba21d94e5eefa516fb60f42237962`
+        )
+        const datos = await respuesta.json();
+         setClima(datos)    
+        
+    }    catch(err){
+        console.log(err)
+    }
 }
 
   return (
     <>
       <Form onSubmit={handleSubmit(onSubmit)}>
         <InputGroup className="mb-3">
-          <InputGroup.Text id="basic-addon1">Ubicacion: </InputGroup.Text>
+          <InputGroup.Text id="basic-addon1">Ciudad: </InputGroup.Text>
           <Form.Control
-            placeholder="Ubicacion"
+            placeholder="Ciudad"
             aria-label="Username"
             aria-describedby="basic-addon1"
-            {...register("ubicacion", {
+            
+            {...register("ciudad", {
                 required: true,                
             })}
           />
@@ -33,6 +101,7 @@ const onSubmit= (data) => {
             placeholder="Pais"
             aria-label="Username"
             aria-describedby="basic-addon1"
+            
             {...register("pais", {
                 required: true
             })
@@ -44,7 +113,10 @@ const onSubmit= (data) => {
         <Button type="submit">Mostrar Clima</Button>
         </div>
       </Form>
-      <p>El clima es: </p>
+      <p>El clima es: {tipodeclima}</p>
+      <p>Viento: direccion: {grados}° , velocidad: {velocidad} mph</p>
+      
+
     </>
   );
 };
